3401905d5e0b6b426abca08b03b95077bf2f48e1
Brian Jacobel <bjacobel@gmail.com>
2016-04-06 16:06:24 -0400

    remove superfluous text! and !strip

--- icon.js
+++ icon.js
@@ -1,7 +1,7 @@
 define(['text'], function (text) {
   return {
     load: function (name, req, onLoad, config) {
-      text.get(req.toUrl('text!edx-ui-toolkit/svg/' + name + '.svg!strip'), function (icon) {
+      text.get(req.toUrl('edx-ui-toolkit/svg/' + name + '.svg'), function (icon) {
         onLoad(icon);
       }, onLoad.error);
     },
